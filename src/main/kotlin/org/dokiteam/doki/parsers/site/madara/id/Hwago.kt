28edb33672a03ec53468683459273f729d7044fa
MuhamadSyabitHidayattulloh <tebepc@gmail.com>
2025-11-16 11:17:38 +0700

    Update (Domain): Hwago Parser

--- src/main/kotlin/org/dokiteam/doki/parsers/site/madara/id/Hwago.kt
+++ src/main/kotlin/org/dokiteam/doki/parsers/site/madara/id/Hwago.kt
@@ -6,9 +6,8 @@ import org.dokiteam.doki.parsers.MangaSourceParser
 import org.dokiteam.doki.parsers.model.MangaParserSource
 import org.dokiteam.doki.parsers.site.madara.MadaraParser
 
-@Broken
 @MangaSourceParser("HWAGO", "Hwago", "id")
 internal class Hwago(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.HWAGO, "hwago01.xyz") {
+	MadaraParser(context, MangaParserSource.HWAGO, "01.hwago.xyz") {
 	override val datePattern = "d MMMM yyyy"
 }
