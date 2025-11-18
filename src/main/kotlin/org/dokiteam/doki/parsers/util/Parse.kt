2187340fc42ceb4165d048b7cca324793c69ec22
dragonx943 <premieregirl26@gmail.com>
2025-11-18 13:28:38 +0700

    Revert all

--- src/main/kotlin/org/dokiteam/doki/parsers/util/Parse.kt
+++ src/main/kotlin/org/dokiteam/doki/parsers/util/Parse.kt
@@ -23,9 +23,8 @@ internal const val SCHEME_HTTPS = "https"
 // TODO suspend
 public fun Response.parseHtml(): Document = use { response ->
     val body = response.body
-    val baseUri = response.request.url.toString()
-    val html = body.string()
-    Jsoup.parse(html, baseUri)
+    val charset = body.contentType()?.charset()?.name()
+    Jsoup.parse(body.byteStream(), charset, response.request.url.toString())
 }
 
 /**
